4923e577371f784eb699ae5028dfa232560208b4
Frank Vasco <askim8610@gmai.com>
2021-04-05 21:46:28 -0500

    Metodo suma y resta

--- src/Calc.java
+++ src/Calc.java
@@ -1,5 +1,20 @@
 public class Calc {
 
+    public static void main(String[] args) {
+        System.out.println("The sum is: " + sum(4,5));
+        System.out.println("The sum is: " + substract(4,5));
+    }
+
+
+    public static int sum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int substract(int a, int b){
+        int substract = a - b;
+        return substract;
+    }
 
 
 
