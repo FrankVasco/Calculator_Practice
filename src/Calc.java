75c6edc9f6a33c31aebc566c5855f8b29631bf4a
Frank Vasco <askim8610@gmai.com>
2021-04-05 21:58:53 -0500

    Agregar division y multiplicacion

--- src/Calc.java
+++ src/Calc.java
@@ -1,6 +1,31 @@
 public class Calc {
 
+    public static void main(String[] args) {
+        System.out.println("The sum is: " + sum(4,5));
+        System.out.println("The subtraction is: " + substract(4,5));
+        System.out.println("The multiplication is: " + multiplication(4,5));
+        System.out.println("The division is: " + division(4,5));
+    }
 
 
+    public static int sum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    public static int substract(int a, int b){
+        int substract = a - b;
+        return substract;
+    }
+
+    public static int multiplication(int a, int b){
+        int multiplication = a * b;
+        return multiplication;
+    }
+
+    public static float division(int a, int b){
+        float division = (float)a / b;
+        return division;
+    }
 
 }
